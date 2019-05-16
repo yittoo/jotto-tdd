d9ae1b31c63f1d3e489c4d45cf6c40c24845766f
yittoo <yittoo@users.noreply.github.com>
2019-05-16 22:57:28 +0300

    Add Thunk for guessedWords piece of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { getLetterMatchCount } from "../helpers";
+
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
   GUESS_WORD: "GUESS_WORD"
@@ -11,7 +13,7 @@ export const correctGuess = () => {
   return {
     type: actionTypes.CORRECT_GUESS
   };
-}
+};
 
 /**
  * Returns Redux Thunk function that dispatches GUESS_WORD action
@@ -20,8 +22,18 @@ export const correctGuess = () => {
  * @param {string} guessedWord - Guessed word
  * @returns {function} - Redux Thunk function
  */
-export const guessWord = (guessedWord) => {
+export const guessWord = guessedWord => {
   return (dispatch, getState) => {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
 
-  }
-}
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    });
+
+    if(guessedWord === secretWord){
+      dispatch({ type: actionTypes.CORRECT_GUESS})
+    }
+  };
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import success from "./successReducer";
 import guessedWords from "./guessWordReducer";
+import secretWord from "./secretWordReducer";
 
-export default combineReducers({ success, guessedWords });
+export default combineReducers({ success, guessedWords, secretWord });
