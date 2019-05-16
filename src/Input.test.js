8ace727d3319f3ea743ffce55aed5fe5201b6ada
yittoo <yittoo@users.noreply.github.com>
2019-05-16 16:59:33 +0300

    write tests for Input success=true

--- src/Input.test.js
+++ src/Input.test.js
@@ -38,9 +38,23 @@ describe("render", () => {
     });
   });
   describe("word has been guessed", () => {
-    it("renders component without error", () => {});
-    it("does not renders input box", () => {});
-    it("does not renders submit button", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
+    it("renders component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    it("does not renders input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(0);
+    });
+    it("does not renders submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(0);
+    });
   });
 });
 
