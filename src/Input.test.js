9a1c61e2061490e3cb240e04f2556961ca8e6c40
yittoo <yittoo@users.noreply.github.com>
2019-05-16 16:56:21 +0300

    Complete Input component render tests for success=false

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {shallow} from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import {findByTestAttr, storeFactory} from '../test/testUtils';
-import Input from './Input';
+import { findByTestAttr, storeFactory } from "../test/testUtils";
+import Input from "./Input";
 
 /**
  * Factory function to create a ShallowWrapper for the GuessedWords component
@@ -12,34 +12,36 @@ import Input from './Input';
  */
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
-  const wrapper = shallow(<Input store={store} />).dive().dive();
-}
+  return shallow(<Input store={store} />)
+    .dive()
+    .dive();
+};
 
-describe('render', () => {
-  describe('word has not been guessed', () => {
-    it('renders component without error', () => {
-
+describe("render", () => {
+  describe("word has not been guessed", () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
     });
-    it('renders input box', () => {
-
-    })
-    it('renders submit button', ()=> {
-
-    })
-  })
-  describe('word has been guessed', () => {
-    it('renders component without error', () => {
-
+    it("renders component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
     });
-    it('does not renders input box', () => {
+    it("renders input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
+    it("renders submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
+    });
+  });
+  describe("word has been guessed", () => {
+    it("renders component without error", () => {});
+    it("does not renders input box", () => {});
+    it("does not renders submit button", () => {});
+  });
+});
 
-    })
-    it('does not renders submit button', ()=> {
-      
-    })
-  })
-})
-
-describe('update state', () => {
-
-})
+describe("update state", () => {});
