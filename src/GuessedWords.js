39fde99098b0f0ae7f917e64a0ca0b63f58deeff
yittoo <yittoo@users.noreply.github.com>
2019-05-16 15:20:58 +0300

    Tests and code for non-zero guessed words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -3,15 +3,34 @@ import PropTypes from "prop-types";
 
 const GuessedWords = props => {
   let contents;
-  if(props.guessedWords.length === 0) {
+  if (props.guessedWords.length === 0) {
     contents = (
       <span data-test="guess-instructions">Try to guess secret word!</span>
-    )
+    );
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => {
+      return (
+        <tr data-test="guessed-word" key={index}>
+          <td>{word.guessedWord}</td>
+          <td>{word.letterMatchCount}</td>
+        </tr>
+      );
+    });
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </div>
+    );
   }
-  return (
-  <div data-test="component-guessed-words">
-    {contents}
-  </div>);
+  return <div data-test="component-guessed-words">{contents}</div>;
 };
 
 GuessedWords.propTypes = {
--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -21,21 +21,41 @@ it("does not throw warning with expected props", () => {
   checkProps(GuessedWords, defaultProps);
 });
 
-describe('if there are no words guessed', () => {
+describe("if there are no words guessed", () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = setup({guessedWords: []});
-  })
-  test('renders without error', () => {
-    const component = findByTestAttr(wrapper, 'component-guessed-words');
-    expect(component.length).toBe(1)
+    wrapper = setup({ guessedWords: [] });
   });
-  test('renders instructions to guess a word', () => {
-    const instructions = findByTestAttr(wrapper, 'guess-instructions');
+  it("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  it("renders instructions to guess a word", () => {
+    const instructions = findByTestAttr(wrapper, "guess-instructions");
     expect(instructions.text().length).not.toBe(0);
   });
 });
 
-describe('if there are words guessed', () => {
-
+describe("if there are words guessed", () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 5 }
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  it("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  it('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1)
+  });
+  it("displays correct number of words", () => {
+    const guessedWordNodes = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordNodes.length).toBe(guessedWords.length)
+  });
 });
